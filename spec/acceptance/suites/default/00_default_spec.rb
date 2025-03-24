7bf54b87e2f99496be4f2fdf6517042869fd719e
Rick Gardner <R.gardner.jr@gmail.com>
2025-03-24 14:42:38 -0400

    updated nodesets and fixed up spec tests

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -70,46 +70,46 @@ describe 'aide class' do
       end
     end
 
-    #context 'with syslog and logrotate enabled' do
-    #  let(:hieradata) {{
-    #    'simp_options::auditd'    => false ,
-    #    'simp_options::syslog'    => true ,
-    #    'simp_options::logrotate' => true,
-    #    'aide::syslog_format'     => true,
-    #    'auditd::enable'          => false,
-    #   }}
-#
-#      it 'should work with no errors' do
-#        set_hieradata_on(host, hieradata)
-#        apply_manifest_on(host, manifest, :catch_failures => true)
-#        # rsyslog changes require a second run
-#        apply_manifest_on(host, manifest, :catch_failures => true)
-#      end
+    context 'with syslog and logrotate enabled' do
+      let(:hieradata) {{
+        'simp_options::auditd'    => false ,
+        'simp_options::syslog'    => true ,
+        'simp_options::logrotate' => true,
+        'aide::syslog_format'     => true,
+        'auditd::enable'          => false,
+       }}
 
-#      it 'should be idempotent' do
-#        apply_manifest_on(host, manifest, :catch_changes => true)
-#      end
+      it 'should work with no errors' do
+        set_hieradata_on(host, hieradata)
+        apply_manifest_on(host, manifest, :catch_failures => true)
+        # rsyslog changes require a second run
+        apply_manifest_on(host, manifest, :catch_failures => true)
+      end
 
-#      it 'should generate an empty or clean report and log nothing when no problems are found' do
-#        on(host, '/usr/local/sbin/update_aide')
-#        on(host, 'logrotate --force /etc/logrotate.simp.d/aide')
-#        on(host, '/usr/sbin/aide --check')
-#        report = on(host, 'cat /var/log/aide/aide.report').stdout
-#        expect(report).to match(/^(.+NO differences.+)?$/)
-#        log = on(host, 'cat /var/log/aide/aide.log').stdout
-#        expect(log).to match(/^(.+NO differences.+)?$/)
-#      end
+      it 'should be idempotent' do
+        apply_manifest_on(host, manifest, :catch_changes => true)
+      end
 
-#      it 'should generate a valid report and log that report when problems are found' do
-#        on(host, 'touch /etc/yum.conf')
-#        on(host, '/usr/sbin/aide --check', :acceptable_exit_codes => changes_detected)
-#
-#        on(host, "grep 'found differences between database and filesystem' /var/log/aide/aide.report")
-#        on(host, "grep '/etc/.*\.conf' /var/log/aide/aide.report")
-#
-#        on(host, "grep 'found differences between database and filesystem' /var/log/aide/aide.log")
-#        on(host, "grep '/etc/.*\.conf' /var/log/aide/aide.log")
-#      end
-#    end
+      it 'should generate an empty or clean report and log nothing when no problems are found' do
+        on(host, '/usr/local/sbin/update_aide')
+        on(host, 'logrotate --force /etc/logrotate.simp.d/aide')
+        on(host, '/usr/sbin/aide --check')
+        report = on(host, 'cat /var/log/aide/aide.report').stdout
+        expect(report).to match(/^(.+NO differences.+)?$/)
+        log = on(host, 'cat /var/log/aide/aide.log').stdout
+        expect(log).to match(/^(.+NO differences.+)?$/)
+      end
+
+      it 'should generate a valid report and log that report when problems are found' do
+        on(host, 'touch /etc/yum.conf')
+        on(host, '/usr/sbin/aide --check', :acceptable_exit_codes => changes_detected)
+
+        on(host, "grep 'found differences between database and filesystem' /var/log/aide/aide.report")
+        on(host, "grep '/etc/.*\.conf' /var/log/aide/aide.report")
+
+        on(host, "grep 'found differences between database and filesystem' /var/log/aide/aide.log")
+        on(host, "grep '/etc/.*\.conf' /var/log/aide/aide.log")
+      end
+    end
   end
 end
